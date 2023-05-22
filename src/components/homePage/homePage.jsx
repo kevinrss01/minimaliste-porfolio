bc02c6f96d981e70e859f32be3dd46892d568094
kevinrss01 <kevin.rousseau3@gmail.com>
2023-05-22 17:26:12 +0200

    progression dans la création de la page principale

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Skils from './Skils'
 import AboutMe from './AboutMe'
 import ExperiencesAndFormations from './ExperiencesAndFormations'
+import { ProgressBar } from '@tremor/react'
 
 const myComponents = [
     {
@@ -16,27 +17,59 @@ const myComponents = [
 ]
 
 export default function HomePage() {
+    const [scrollLevel, setScrollLevel] = useState(0)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollLevel = window.pageYOffset || document.documentElement.scrollTop
+            setScrollLevel(currentScrollLevel)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <div
-            className='homePage-container flex-centre'
-            style={{
-                flexDirection: 'column',
-            }}
-        >
-            {myComponents.map((component, index) => {
-                return (
-                    <div
-                        className='flex-centre'
-                        style={{
-                            width: '100%',
-                            flexDirection: 'column',
-                        }}
-                        key={index}
-                    >
-                        {component.comp}
-                    </div>
-                )
-            })}
-        </div>
+        <>
+            <div
+                style={{
+                    position: 'fixed',
+                    width: '40%',
+                    top: '50%',
+                    rotate: '90deg',
+                    right: '-100px',
+                }}
+            >
+                <ProgressBar
+                    percentageValue={(scrollLevel * 100) / 651}
+                    style={{ height: '50px' }}
+                />
+            </div>
+
+            <div
+                className='homePage-container flex-centre'
+                style={{
+                    flexDirection: 'column',
+                }}
+            >
+                {myComponents.map((component, index) => {
+                    return (
+                        <div
+                            className='flex-centre'
+                            style={{
+                                width: '100%',
+                                flexDirection: 'column',
+                            }}
+                            key={index}
+                        >
+                            {component.comp}
+                        </div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
